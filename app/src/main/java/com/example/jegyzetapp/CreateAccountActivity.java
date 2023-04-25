c116ad2cc20128a31bd85c1cddae516cbc7eb382
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-04-25 16:15:34 +0200

    created register page

--- app/src/main/java/com/example/jegyzetapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/jegyzetapp/CreateAccountActivity.java
@@ -1,14 +1,113 @@
 package com.example.jegyzetapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        createAccountBtn.setOnClickListener(v -> createAccount());
+        loginBtnTextView.setOnClickListener(v -> finish());
+    }
+
+    void createAccount() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email, password, confirmPassword);
+        if (!isValidated) {
+            return;
+        }
+
+        createAccountInFirebase(email, password);
+    }
+
+    void createAccountInFirebase(String email, String password) {
+        ChangeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        ChangeInProgress(false);
+
+                        if (task.isSuccessful()) {
+                            // success
+                            Toast.makeText(CreateAccountActivity.this, "Sikeres regisztráció!\nEmail megerősítése szükséges.", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+
+                        } else {
+                            // failure
+                            Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    void ChangeInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email, String password, String confirmPassword) {
+        // validate the data that are input by user
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Hibás email cím!");
+            return false;
+        }
+
+        if (password.length() < 6) {
+            passwordEditText.setError("A jelszó túl rövid!");
+            return false;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            confirmPasswordEditText.setError("A két jelszó nem egyezik!");
+            return false;
+        }
+        return true;
     }
 }
