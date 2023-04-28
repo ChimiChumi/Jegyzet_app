57158b51edea120281bd06334821330b1b994945
Dobos Hunor <h044104@stud.u-szeged.hu>
2023-04-28 22:00:28 +0200

    notes saved to firebase

--- app/src/main/java/com/example/jegyzetapp/NoteDetailsActivity.java
+++ app/src/main/java/com/example/jegyzetapp/NoteDetailsActivity.java
@@ -1,10 +1,16 @@
 package com.example.jegyzetapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetailsActivity extends AppCompatActivity {
 
@@ -30,5 +36,30 @@ public class NoteDetailsActivity extends AppCompatActivity {
             titleEditText.setError("A cím nem lehet üres!");
             return;
         }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    void saveNoteToFirebase(Note note){
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    // note is added
+                    Utility.showToast(NoteDetailsActivity.this, "Jegyzet sikeresen hozzáadva");
+                    finish();
+                }
+                else{
+                    Utility.showToast(NoteDetailsActivity.this, task.getException().getLocalizedMessage());
+                }
+            }
+        });
     }
 }
